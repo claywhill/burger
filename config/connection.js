3d1c1472d22e895639978f5b0ab67d9d5b6ceddb
Clayton Hill <clayman0123@yahoo.com>
2020-05-01 16:10:38 -0600

    created router and orm code

--- config/connection.js
+++ config/connection.js
@@ -8,11 +8,12 @@ var connection = mysql.createConnection(({
     database: "burgers_db"
 }));
 
-connection.connect();
+connection.connect(function(err) {
+    if (err) {
+      console.error("error connecting: " + err.stack);
+      return;
+    }
+    console.log("connected as id " + connection.threadId);
+  });
 
-connection.query("SELECT * FROM burgers", function(err, res, fields) {
-    if (err) throw err;
-    console.log(res);
-});
-
-connection.end();
+module.exports = connection;
---
+++ models/burger.js
@@ -0,0 +1,26 @@
+var orm = require("../orm");
+
+var burger = {
+    all: function(callback) {
+        orm.all("burgers", function(res) {
+            callback(res);
+        })
+    },
+    create: function(cols, vals, callback) {
+        orm.create("burgers", cols, vals, function(res) {
+            callback(res);
+        })
+    },
+    update: function(objColVals, condition, callback) {
+        orm.update("burgers", objColVals, condition, function(res) {
+            callback(res);
+        })
+    },
+    delete: function(condition, callback) {
+        orm.delete("burgers", condition, function(res) {
+            callback(res);
+        })
+    }
+}
+
+module.exports = burger;
