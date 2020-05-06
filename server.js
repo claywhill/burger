73ff2cf06c49d89ea4a36371860763557656c065
Clayton Hill <clayman0123@yahoo.com>
2020-05-06 15:14:40 -0600

    install body parser package, required in server.js

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express = require("express");
+var bodyParser = require("body-parser");
 
 var PORT = process.env.PORT || 3000;
 
@@ -6,8 +7,8 @@ var app = express();
 
 app.use(express.static("public"));
 
-app.use(express.urlencoded({ extended: true}));
-app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 var exphbs = require("express-handlebars");
 
